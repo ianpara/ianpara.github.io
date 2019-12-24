97240cba62b7759b86c459c58f0ad56ddafb8069
ianpara <imp2468@gmail.com>
2019-12-23 23:49:39 -0500

    loader

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 10;
+        var id = setInterval(frame, 50);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+                window.open("other.html", "_self");
+
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML = width + "%";
+            }
+
+        }
+    }
+}
