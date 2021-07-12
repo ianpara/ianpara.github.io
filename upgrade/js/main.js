4e9266794be2dcd7d1936af1ec3cdd499318e626
ianpara <imp2468@gmail.com>
2021-07-11 22:39:43 -0400

    upgrade

---
+++ upgrade/js/main.js
@@ -0,0 +1,11 @@
+/*=========================================================================
+Move content on menu toggle on mobile
+=========================================================================*/
+var sideNav = document.getElementById('sideNav');
+var mainContent = document.querySelector('.main-content');
+sideNav.addEventListener('shown.bs.collapse', function () {
+    mainContent.classList.add("push");; 
+})
+sideNav.addEventListener('hide.bs.collapse', function () {
+    mainContent.classList.remove("push");; 
+})
