5a6381f461fec0465620f9c00513f9bd70faa480
ianpara <imp2468@gmail.com>
2021-07-13 16:21:57 -0500

    scrollspy

--- upgrade/js/main.js
+++ upgrade/js/main.js
@@ -26,6 +26,37 @@ toggle.addEventListener("click", function () {
     }
 });
 
+/*=========================================================================
+Fixing BS Scrollspy
+=========================================================================*/
+function hotfixScrollSpy() {
+    var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+    let curScroll = getCurrentScroll();
+    dataSpyList.forEach(function (dataSpyEl) {
+        let offsets = bootstrap.ScrollSpy.getInstance(dataSpyEl)['_offsets'];
+        for(let i = 0; i < offsets.length; i++){
+            offsets[i] += curScroll;
+        }
+    })
+}
+
+function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+}
+
+window.onload = function () {
+    hotfixScrollSpy();
+    window.scrollBy(0,1);
+}
+
+window.addEventListener('resize', function(event) {
+    const dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'));
+    dataSpyList.forEach(function(dataSpyEl) {
+      bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh();
+    });
+    hotfixScrollSpy();
+    window.scrollBy(0, 1);
+}, true);
 
 /*=========================================================================
 Move content on menu toggle on mobile
