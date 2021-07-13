3775c3e605afda0499be0c1c3e57b5314812111c
ianpara <imp2468@gmail.com>
2021-07-13 11:37:21 -0500

    skills, portfolio, dark mode

--- upgrade/js/dist/main.dev.js
+++ upgrade/js/dist/main.dev.js
@@ -1,8 +1,32 @@
 "use strict";
 
 /*=========================================================================
+Set darkmode via OS or local storage
+=========================================================================*/
+document.addEventListener('DOMContentLoaded', function () {
+  if (localStorage.getItem('color-mode') === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches && !localStorage.getItem('color-mode')) {
+    document.documentElement.setAttribute("color-mode", "dark");
+    document.getElementById("dark-toggle").checked = true;
+  }
+});
+/*=========================================================================
+Darkmode Toggle
+=========================================================================*/
+
+var toggle = document.getElementById("dark-toggle");
+toggle.addEventListener("click", function () {
+  if (toggle.checked) {
+    document.documentElement.setAttribute("color-mode", "dark");
+    localStorage.setItem("color-mode", "dark");
+  } else if (!toggle.checked) {
+    document.documentElement.setAttribute("color-mode", "light");
+    localStorage.setItem("color-mode", "light");
+  }
+});
+/*=========================================================================
 Move content on menu toggle on mobile
 =========================================================================*/
+
 var sideNav = document.getElementById('sideNav');
 var mainContent = document.querySelector('.main-content');
 sideNav.addEventListener('shown.bs.collapse', function () {
--- upgrade/js/main.js
+++ upgrade/js/main.js
@@ -1,4 +1,33 @@
 /*=========================================================================
+Set darkmode via OS or local storage
+=========================================================================*/
+document.addEventListener('DOMContentLoaded',() => {
+    if (
+        localStorage.getItem('color-mode') === 'dark' || (window.matchMedia('(prefers-color-scheme: dark)').matches &&
+            !localStorage.getItem('color-mode'))
+        ) {
+        document.documentElement.setAttribute("color-mode", "dark");
+        document.getElementById("dark-toggle").checked = true;
+        }
+});
+
+
+/*=========================================================================
+Darkmode Toggle
+=========================================================================*/
+var toggle = document.getElementById("dark-toggle");
+toggle.addEventListener("click", function () {
+    if (toggle.checked ) {
+        document.documentElement.setAttribute("color-mode", "dark");
+        localStorage.setItem("color-mode", "dark");
+    } else if (!toggle.checked) {
+        document.documentElement.setAttribute("color-mode", "light");
+        localStorage.setItem("color-mode", "light");
+    }
+});
+
+
+/*=========================================================================
 Move content on menu toggle on mobile
 =========================================================================*/
 var sideNav = document.getElementById('sideNav');
